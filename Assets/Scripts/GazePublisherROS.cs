5e94f77bfe5c3109ce9ac55f279b8c6e47e85735
ALaws0n <106745609+ALaws0n@users.noreply.github.com>
2023-03-22 07:11:45 -0400

    Improved Gaze Publisher

--- Assets/Scripts/GazePublisherROS.cs
+++ Assets/Scripts/GazePublisherROS.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Microsoft.MixedReality.Toolkit;
 using UnityEngine;
 using Unity.Robotics.ROSTCPConnector;
 using RosMessageTypes.UnityRoboticsDemo;
@@ -7,9 +8,14 @@ using RosMessageTypes.UnityRoboticsDemo;
 public class GazePublisherROS : MonoBehaviour
 {
     ROSConnection ros;
-    public string topicName = 'Gaze_Pos';
+    public string topicName = "Gaze_Pos";
     public float publishMessageFrequency = 0.1f;
     private float timeElapsed;
+    private float rotX = 0;
+    private float rotY = 0;
+    private float rotZ = 0;
+    private float rotW = 0;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -26,10 +32,13 @@ public class GazePublisherROS : MonoBehaviour
         if(timeElapsed > publishMessageFrequency)
         {
             PosRotMsg gazePos = new PosRotMsg(
-                //What do I log exactly?
                 CoreServices.InputSystem.GazeProvider.GazeDirection.x,
                 CoreServices.InputSystem.GazeProvider.GazeDirection.y,
-                CoreServices.InputSystem.GazeProvider.GazeDirection.z
+                CoreServices.InputSystem.GazeProvider.GazeDirection.z,
+                rotX,
+                rotY,
+                rotZ,
+                rotW
                 );
 
             ros.Publish(topicName, gazePos);
